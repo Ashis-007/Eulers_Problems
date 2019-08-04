9bc9a7f7cdcd45ffb68170f4434105a372dd21a4
Ashis-007 <48730894+Ashis-007@users.noreply.github.com>
2019-08-04 14:03:34 +0530

    Update Problem_5.cpp

--- Problem_5.cpp
+++ Problem_5.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int LCM(int a, int b)
+{
+  int lcm;
+
+  for(lcm = min(a,b) ; lcm <= a * b; lcm++)
+      {
+        if(lcm % a == 0 && lcm % b == 0)
+          return lcm;
+      }
+
+}
+
+
 int main()
 {
 
 	int num = 1;
-	int rem = 0;
 
-	for(int i = 1; i <= 20; i++)
-	{
-		if(i == 20)
-			break;
-
-		rem = num % i;
-
-        if(rem != 0)
-		{
-			++num;
-			i = 1;
-			continue;
-		}
-
+	for(int i = 2; i <= 20; i++)
+    {
+        num = LCM(num, i);
     }
 
 	cout<<"The lcm is: "<<num;
